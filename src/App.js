e163354e7cc508790ee7c2621d81d9be7e59bb38
Mukaila Semiu <semiteagain@gmail.com>
2021-02-03 18:45:27 +0100

    changing the css

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import React, {useState} from 'react';
-import Data from './components/Data';
-import EventsList from './components/eventList'
+import React, { useState } from "react";
+import Data from "./components/Data";
+import EventsList from "./components/eventList";
 
 function App() {
-   const eventNow = new Date().toDateString();
+  const eventNow = new Date().toDateString();
 
-   const [details, setDetails] = useState(Data);
+  const [details, setDetails] = useState(Data);
 
-   function showList() {
-       
-   }
+  function showList(e) {
+    const clearList = e.target.value;
+    setDetails(clearList);
+  }
 
-    return (
-        <main>
-            <section className='container'>
-            <h1>{details.length}Events this week</h1>
-            <h3>{eventNow}</h3>
-             <EventsList details={details}/>
-            <button onClick={()=>setDetails([])}>View all events</button>
-            </section>
-            
-        </main>
-    )
+  return (
+    <main>
+      <section className="container">
+        <div className="heading">
+          <h1>Events List</h1>
+        </div>
+        <h1>{details.length}Events this week</h1>
+        <h3>{eventNow}</h3>
+        <EventsList details={details} />
+        <button onClick={showList}>View all events</button>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
