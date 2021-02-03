a14a6d3abf57a53faa26ab9c5a076433a898687c
Mukaila Semiu <semiteagain@gmail.com>
2021-02-03 18:53:32 +0100

    adding input

---
+++ src/components/InputArea.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function InputArea() {
+    return (
+        <div className='form'>
+        <input />
+        <input/>
+        </div>
+    )
+}
+
+export default InputArea
