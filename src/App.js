3d56c25825a54befe486b777f25a3482bbf79cc6
Mukaila Semiu <semiteagain@gmail.com>
2021-02-03 18:19:25 +0100

    clearing the list

--- src/App.js
+++ src/App.js
@@ -7,13 +7,17 @@ function App() {
 
    const [details, setDetails] = useState(Data);
 
+   function showList() {
+       
+   }
+
     return (
         <main>
             <section className='container'>
             <h1>{details.length}Events this week</h1>
             <h3>{eventNow}</h3>
              <EventsList details={details}/>
-            <button>View all events</button>
+            <button onClick={()=>setDetails([])}>View all events</button>
             </section>
             
         </main>
