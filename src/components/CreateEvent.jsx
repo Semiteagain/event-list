814fbd7fbed782a77714c3d5419a5eced0aa4c08
Mukaila Semiu <semiteagain@gmail.com>
2021-02-10 20:38:41 +0100

    get current date

--- src/components/CreateEvent.jsx
+++ src/components/CreateEvent.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
+
+
+const date = new Date();
+const eventDay = date.toLocaleTimeString()
 function CreateEvent() {
     return (
         <div>
             <form>
+                <h1>{eventDay}</h1>
                 <input type="text" name="title" id="title" placeholder="set event"/>
-                <textarea name="content" id="content" cols="30" rows="10" placeholder="enter details"></textarea>
-                
+                <input type="time" name="eventtime" id="eventtime"/>                
                 <button type="submit">Set Event</button>
             </form>
         </div>
