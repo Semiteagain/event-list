01595ed928509c81732f6ee324c69a91cf98a346
Mukaila Semiu <semiteagain@gmail.com>
2021-02-11 05:44:22 +0100

    add all

--- src/components/CreateEvent.jsx
+++ src/components/CreateEvent.jsx
@@ -1,16 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 
 
-const date = new Date();
-const eventDay = date.toLocaleTimeString()
+
 function CreateEvent() {
+
+
+    const [todo, setTodo ] = useState ({
+        title : '',
+        content : '',
+        date : ''
+    });
+
+
+
+    function handleChange(event) {
+        const {name, value} = event.target;
+
+        setTodo(prevTodo =>{
+            return {
+                ...prevTodo,
+                [name] : value
+            }
+        })
+    }
     return (
         <div>
-            <form>
-                <h1>{eventDay}</h1>
-                <input type="text" name="title" id="title" placeholder="set event"/>
-                <input type="time" name="eventtime" id="eventtime"/>                
+            <form className="create-event">
+                <h3>Create Event</h3>
+                <input 
+                type="text" 
+                name="title" 
+                value={todo.title}
+                onChange={handleChange}
+                placeholder="set event"/>
+                <textarea 
+                name="content" 
+                onChange={handleChange}
+                value={todo.content}
+                rows="3"
+                />
+                       
                 <button type="submit">Set Event</button>
             </form>
         </div>
