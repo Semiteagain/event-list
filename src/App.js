2f0f1ec7d7bdf969601fccee1c8dd55bf82bb156
Mukaila Semiu <semiteagain@gmail.com>
2021-02-12 05:40:06 +0100

    Add input

--- src/App.js
+++ src/App.js
@@ -1,41 +1,75 @@
 import React, { useState } from "react";
-import Data from "./components/Data";
-import EventsList from "./components/eventList";
-import CreateEvent from './components/CreateEvent'
+// import Data from "./components/Data";
+// import EventsList from "./components/eventList";
+// import CreateEvent from './components/CreateEvent'
 
 
 function App() {
-  const eventNow = new Date().toDateString();
+  // const eventNow = new Date().toDateString();
 
-  const [details, setDetails] = useState(Data);
+  const [newList, setNewList] = useState({
+    title : "",
+    content : "",
 
-  function showList(e) {
-    const clearList = e.target.value;
-    setDetails(clearList);
+  });
+
+  function handleChange(e) {
+    const {name, value} = e.target.value
+    setNewList(prevEvent => {
+      return {
+        ...prevEvent,
+        [name] : value
+      }
+    });
   }
 
+  addToList ()
+
   return (
-    <main>
-      <section className="container">
-        <div className="heading">
-          <h1>Events List</h1>
-        
-        </div>
-        
-        <h1>{details.length}Events this week</h1>
-        <h3>{eventNow}</h3>
-        <EventsList details={details} />
-        <button onClick={showList}>View all events</button>
-      </section>
+
+     <main>
+       <section className="container">
+            Add new Event ...
+            <br/>
+            <input
+            type="text"
+            placeholder="Enter event here..."
+            value={newList.title}
+            onChange={handleChange}
+            
+            />
 
 
-      <side>
-        <CreateEvent />
-      </side>
-    </main>
+            <button onClick={addToList}>Add Event</button>
+       </section>
+
+     
+     </main>
+
+
+
+
+    // <main>
+    //   <section className="container">
+    //     <div className="heading">
+    //       <h1>Events List</h1>
+        
+    //     </div>
+        
+    //     <h1>{details.length}Events this week</h1>
+    //     <h3>{eventNow}</h3>
+    //     <EventsList details={details} />
+    //     <button onClick={showList}>View all events</button>
+    //   </section>
+
+
+    //   <side>
+    //     <CreateEvent />
+    //   </side>
+    // </main>
 
   
-  );
+  )
 }
 
 export default App;
